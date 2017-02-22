6df4eae87728a087dbece2ccc5f3172100a3600e
jmmunoz86 <jmmunoz86@gmail.com>
2017-02-22 21:48:09 +0100

    removing unnecessary files

--- raspberry/test_json_reader.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('experiment_settings.json','r') as f:
-    settings = f.read().replace('\n', ' ').replace('\r', '')
-    settings = json.loads(settings)
-print settings
-
-raw_input('done')
