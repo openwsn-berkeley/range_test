2f0c71d7881b7bb19174c596fb4d7c8966760fac
jmmunoz86 <jmmunoz86@gmail.com>
2017-05-03 16:27:56 +0200

    TX power to maximum

--- raspberry/experiment_rx.py
+++ raspberry/experiment_rx.py
@@ -334,8 +334,8 @@ class ExperimentRx(threading.Thread):
 
         self.radio_setup()
         logging.warning('WAITING FOR THE START BUTTON TO BE PRESSED')
-        # self.start_experiment.wait()
-        # self.start_experiment.clear()
+        self.start_experiment.wait()
+        self.start_experiment.clear()
         self.started_time = time.time()
         self.hours, self.minutes = self.time_experiment()
         self.time_to_start = dt.combine(dt.now(), datetime.time(self.hours, self.minutes))
